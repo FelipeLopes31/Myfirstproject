34ce9b16e4d3b272a77112a185ee8d61b86d205b
unknown <felipewilderlopes@gmail.com>
2022-05-25 16:19:20 -0300

    projeto pronto com o banco mysql

--- src/main/java/com/devsuperior/myfirstproject/MyfirstprojectApplication.java
+++ src/main/java/com/devsuperior/myfirstproject/MyfirstprojectApplication.java
@@ -1,49 +1,20 @@
 package com.devsuperior.myfirstproject;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.devsuperior.myfirstproject.domain.Category;
-import com.devsuperior.myfirstproject.domain.Product;
-import com.devsuperior.myfirstproject.repositories.CategoryRepository;
-import com.devsuperior.myfirstproject.repositories.ProductRepository;
-
 @SpringBootApplication
-public class MyfirstprojectApplication implements CommandLineRunner {
+public class MyfirstprojectApplication  {
 
-	@Autowired
-	private CategoryRepository categoryRepository;
-	
-	@Autowired
-	private ProductRepository productRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstprojectApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Category cat1 = new Category(null, "Books",null);
-		Category cat2 = new Category(null, "Eletronics",null);
 
-		Product p1 = new Product(null, "TV", 2200.00, cat1);
-		Product p2 = new Product(null, "Devotional Joy of Dawn", 53.60, cat2);
-		Product p3 = new Product(null, "PS5", 4999.99, cat1);
-		Product p4 = new Product(null, "Twelve days to update your life", 32.49, cat2);
-
-		cat1.getProduct().addAll(Arrays.asList(p1, p3));
-		cat2.getProduct().addAll(Arrays.asList(p2, p4));
-		categoryRepository.save(cat1);
-		categoryRepository.save(cat2);
+	
 		
-		productRepository.save(p1);
-		productRepository.save(p2);
-		productRepository.save(p3);
-		productRepository.save(p4);
-	}
+		
+	
 
 }
